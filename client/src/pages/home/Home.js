325869e301db374f3f0a8b16cce953ce0a6429a1
brogers856 <benrogers856@gmail.com>
2021-10-01 15:57:19 -0400

    Finished home page

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -5,59 +5,51 @@ import './Home.css'
 const { Title } = Typography;
 
 const Home = () => {
-    return (
-      <>
+  return (
+    <>
       <Row type="flex" justify="center" align="middle">
         <Col span={8}>
           <Title id="pageTitle" justify="center">WELCOME TO OLDEGG</Title>
         </Col>
       </Row>
-      <Row className="card-container" type="flex" justify="space-between" >
-          <Col span={10}>
-            <Card id="missionCard" title="OUR MISSION">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus auctor urna nulla, id porttitor tellus bibendum id.
-                Quisque dictum erat non purus luctus, id tincidunt ipsum pulvinar. Praesent at nunc eu nisl maximus euismod. Donec tincidunt
-                enim vel lectus imperdiet interdum. Ut feugiat efficitur volutpat. Praesent tempus mauris ac condimentum lacinia. Nullam in
-                lobortis ligula. Cras quis ante aliquam, euismod sem sit amet, condimentum odio. Pellentesque dui nulla, varius vitae convallis
-                vel, aliquet faucibus elit. Aliquam at tellus maximus metus volutpat porta. Donec ac rutrum sapien.
-              </p>
-            </Card>
-          </Col>
-          <Col span={10}>
-            <Card id="visionCard" title="OUR VISON">
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus auctor urna nulla, id porttitor tellus bibendum id.
-                Quisque dictum erat non purus luctus, id tincidunt ipsum pulvinar. Praesent at nunc eu nisl maximus euismod. Donec tincidunt
-                enim vel lectus imperdiet interdum. Ut feugiat efficitur volutpat. Praesent tempus mauris ac condimentum lacinia. Nullam in
-                lobortis ligula. Cras quis ante aliquam, euismod sem sit amet, condimentum odio. Pellentesque dui nulla, varius vitae convallis
-                vel, aliquet faucibus elit. Aliquam at tellus maximus metus volutpat porta. Donec ac rutrum sapien.
-              </p>
-            </Card>
-          </Col>
-        </Row>
-        <Row className="card-container" type="flex" justify="center">
-          <Col span={16}>
-          <Title level={3} class="blurb-title" justify="start">OUR STRATEGY</Title>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus auctor urna nulla, id porttitor tellus bibendum id.
-                Quisque dictum erat non purus luctus, id tincidunt ipsum pulvinar. Praesent at nunc eu nisl maximus euismod. Donec tincidunt
-                enim vel lectus imperdiet interdum. Ut feugiat efficitur volutpat. Praesent tempus mauris ac condimentum lacinia. Nullam in
-                lobortis ligula. Cras quis ante aliquam, euismod sem sit amet, condimentum odio. Pellentesque dui nulla, varius vitae convallis
-                vel, aliquet faucibus elit. Aliquam at tellus maximus metus volutpat porta. Donec ac rutrum sapien.
-              </p>
-          </Col>
-        </Row>
-        <Row className="card-container" type="flex" justify="center">
-          <Col span={16}>
-          <Title level={3} class="blurb-title" justify="start">OUR MESSAGE TO YOU</Title>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus auctor urna nulla, id porttitor tellus bibendum id.
-                Quisque dictum erat non purus luctus, id tincidunt ipsum pulvinar. Praesent at nunc eu nisl maximus euismod. Donec tincidunt
-                enim vel lectus imperdiet interdum. Ut feugiat efficitur volutpat. Praesent tempus mauris ac condimentum lacinia. Nullam in
-                lobortis ligula. Cras quis ante aliquam, euismod sem sit amet, condimentum odio. Pellentesque dui nulla, varius vitae convallis
-                vel, aliquet faucibus elit. Aliquam at tellus maximus metus volutpat porta. Donec ac rutrum sapien.
-              </p>
-          </Col>
-        </Row>
-      </>
-    )
+      <Row className="card-container" type="flex" justify="center" align="middle">
+        <Col span={20}>
+          <Card id="missionCard">
+            <Row type="flex" justify="center">
+              <Col span={11}>
+                <Title level={3} className="blurb-title" justify="start">OUR MISSION</Title>
+                <p display="block">To provide our customers with affordable refurbished hardware, from individual components to full computers</p>
+                <Title level={3} className="blurb-title" justify="start">OUR VISION</Title>
+                <p display="block">Our vision is to provide an affordable entry point to the world of computer hardware</p>
+              </Col>
+              <Col span={2}></Col>
+              <Col span={11}>
+                <img src="https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1053&q=80" />
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+      </Row>
+      <Row className="card-container" type="flex" justify="center" align="middle">
+        <Col span={20}>
+          <Card id="missionCard">
+            <Row type="flex" justify="center">
+              <Col span={11}>
+                <img src="https://images.unsplash.com/photo-1591238372338-22d30c883a86?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1171&q=80" />
+              </Col>
+              <Col span={2}></Col>
+              <Col span={11}>
+                <Title level={3} class="blurb-title" justify="start">OUR STRATEGY</Title>
+                <p>Our strategy is to use excellent customer service and easy access to affordable components to establish a loyal customer base</p>
+                <Title level={3} class="blurb-title" justify="start">OUR MESSAGE TO YOU</Title>
+                <p>We hope that you can find whatever you need on our site at a reasonable price, and that you give us your feedback!</p>
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  )
 }
 
 export default Home;
