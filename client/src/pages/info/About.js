465edbb0743cab6afdb4a2c4e36b74f400888a73
Logan Wolfe <Lmwolfe19@gmail.com>
2021-10-03 21:48:26 -0400

    Completed about me section

--- client/src/pages/info/About.js
+++ client/src/pages/info/About.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Row, Col, Space } from "antd";
+import { Card, Row, Col } from "antd";
 
 const About = () => {
     
@@ -7,14 +7,18 @@ const About = () => {
         <>
             <h1  style={{display: 'flex',  justifyContent:'center'}}>Meet the Team</h1>
             <Row style={{marginTop: "1rem", marginBottom: "1rem"}} justify="center">
-                <Col span={12}>
+                <Col span={13}>
                     <Card size="large" title="Logan Wolfe" extra={<b>Co-CEO</b>}>
-                        <p>Test</p>
+                        <p>I am a senior studying Computer Science and Engineering at The Ohio State University.
+                        My current interests lie primarily in ML/AI, DevOps, and infrastructure. Most of my
+                        experience is in Java and Python but I am confident with C and JavaScript as well. My
+                        goal is to become an AI/ML engineer or a system architect once I graduate.
+                        </p>
                     </Card>
                 </Col>
             </Row>
             <Row style={{marginTop: "1rem", marginBottom: "1rem"}} justify="center">
-                <Col span={12}>
+                <Col span={13}>
                     <Card size="large" title="Ben Rogers" extra={<b>Co-CEO</b>}>
                         <p>I am a graduating senior majoring in Computer Information Science. 
                         I have experience in many languages and frameworks, but I am most confident with Java, C#, and JavaScript.
