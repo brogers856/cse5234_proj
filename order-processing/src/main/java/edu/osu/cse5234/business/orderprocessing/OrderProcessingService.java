c2ce1d23b53d4c72fae1b52ba1fe8e99aa6498c3
brogers856 <rogers.1264@buckeyemail.osu.edu>
2021-10-12 19:30:34 -0400

    Added interaction between microservices and fixes CORS

--- order-processing/src/main/java/edu/osu/cse5234/business/orderprocessing/OrderProcessingService.java
+++ order-processing/src/main/java/edu/osu/cse5234/business/orderprocessing/OrderProcessingService.java
@@ -1,5 +1,7 @@
 package edu.osu.cse5234.business.orderprocessing;
 
+import java.io.IOException;
+
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
 import javax.json.bind.JsonbConfig;
@@ -9,12 +11,15 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.Path;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+
 
 @Path("order")
 public class OrderProcessingService {
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response submitOrder(String jsonOrder) {
+	public Response submitOrder(String jsonOrder) throws IOException {
 		JsonbConfig config = new JsonbConfig()
 		.withPropertyNamingStrategy(PropertyNamingStrategy.CASE_INSENSITIVE);
 		
@@ -22,8 +27,15 @@ public class OrderProcessingService {
 		Order submittedOrder = jsonb.fromJson(jsonOrder, Order.class);
 
 		for (Item itemInCart : submittedOrder.getCart()) {
-			if (itemInCart.getQuantity() > itemInCart.getAvailableQuantity()) {
-				return Response.status(418, "Insufficient Quantity: " + itemInCart.getProduct()).build();
+			Client client = ClientBuilder.newClient();
+			Response response = client.target("http://localhost:9080/inventory-management/inventory/items")
+					.path(String.valueOf(itemInCart.getKey()))
+					.request(MediaType.APPLICATION_JSON)
+					.get(Response.class);
+			String body = response.readEntity(String.class);
+			Item item = jsonb.fromJson(body, Item.class);
+			if (itemInCart.getQuantity() > item.getAvailableQuantity()) {
+				return Response.status(418).entity("Insufficient Quantity: " + itemInCart.getProduct()).build();
 			}
 		}
 		
