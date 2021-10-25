c2cef14487a19e3522db3843296f777db596ead9
Logan Wolfe <Lmwolfe19@gmail.com>
2021-10-24 23:24:19 -0400

    Added order peristing

--- order-processing/src/main/java/edu/osu/cse5234/business/orderprocessing/OrderProcessingService.java
+++ order-processing/src/main/java/edu/osu/cse5234/business/orderprocessing/OrderProcessingService.java
@@ -2,6 +2,7 @@ package edu.osu.cse5234.business.orderprocessing;
 
 import java.io.IOException;
 
+import javax.inject.Inject;
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
 import javax.json.bind.JsonbConfig;
@@ -10,13 +11,21 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import org.bson.Document;
+
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoCollection;
+
 import javax.ws.rs.Path;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 
-
 @Path("order")
 public class OrderProcessingService {
+	@Inject
+	MongoDatabase db;
+	
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response submitOrder(String jsonOrder) throws IOException {
@@ -28,16 +37,21 @@ public class OrderProcessingService {
 
 		for (Item itemInCart : submittedOrder.getCart()) {
 			Client client = ClientBuilder.newClient();
-			Response response = client.target("http://localhost:9080/inventory-management/inventory/items")
+			Response inventory = client.target("http://localhost:9080/inventory-management/inventory/items")
 					.path(String.valueOf(itemInCart.getKey()))
 					.request(MediaType.APPLICATION_JSON)
 					.get(Response.class);
-			String body = response.readEntity(String.class);
-			Item item = jsonb.fromJson(body, Item.class);
-			if (itemInCart.getQuantity() > item.getAvailableQuantity()) {
+			
+			Item itemInventory = jsonb.fromJson(inventory.readEntity(String.class), Item.class);
+			
+			if (itemInCart.getQuantity() > itemInventory.getAvailableQuantity()) {
 				return Response.status(418).entity("Insufficient Quantity: " + itemInCart.getProduct()).build();
-			}
+			}			
 		}
+		
+		MongoCollection<Document> ordersMongoDBCollection = db.getCollection("orders");
+		Document orderDetails = Document.parse(jsonOrder);
+		ordersMongoDBCollection.insertOne(orderDetails);
 		
 		return Response.status(200).entity("Order accepted!").build();
 	}
